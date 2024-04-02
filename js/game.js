e8686fee3eb6f3fe94e6ac595b8d14c5c1dc5afd
Estebanxyac10 <esteban.chiquet@epitech.eu>
2024-04-02 14:30:17 +0200

    [+] step 2

--- js/game.js
+++ js/game.js
@@ -32,7 +32,7 @@
 
             // initialisation
             this.grid = document.querySelector('.board .main-grid');
-            this.miniGrid = document.querySelector('.board .mini-grid');
+            this.miniGrid = document.querySelector('.mini-grid');
 
             // défini l'ordre des phase de jeu
             this.phaseOrder = [
@@ -76,30 +76,30 @@
             }
 
             switch (this.currentPhase) {
-            case this.PHASE_GAME_OVER:
-                // detection de la fin de partie
-                if (!this.gameIsOver()) {
-                    // le jeu n'est pas terminé on recommence un tour de jeu
-                    this.currentPhase = this.phaseOrder[this.playerTurnPhaseIndex];
-                }
-            case this.PHASE_INIT_PLAYER:
-                utils.info("Placez vos bateaux");
-                break;
-            case this.PHASE_INIT_OPPONENT:
-                this.wait();
-                utils.info("En attente de votre adversaire");
-                this.players[1].isShipOk(function () {
-                    self.stopWaiting();
-                    self.goNextPhase();
-                });
-                break;
-            case this.PHASE_PLAY_PLAYER:
-                utils.info("A vous de jouer, choisissez une case !");
-                break;
-            case this.PHASE_PLAY_OPPONENT:
-                utils.info("A votre adversaire de jouer...");
-                this.players[1].play();
-                break;
+                case this.PHASE_GAME_OVER:
+                    // detection de la fin de partie
+                    if (!this.gameIsOver()) {
+                        // le jeu n'est pas terminé on recommence un tour de jeu
+                        this.currentPhase = this.phaseOrder[this.playerTurnPhaseIndex];
+                    }
+                case this.PHASE_INIT_PLAYER:
+                    utils.info("Placez vos bateaux");
+                    break;
+                case this.PHASE_INIT_OPPONENT:
+                    this.wait();
+                    utils.info("En attente de votre adversaire");
+                    this.players[1].isShipOk(function () {
+                        self.stopWaiting();
+                        self.goNextPhase();
+                    });
+                    break;
+                case this.PHASE_PLAY_PLAYER:
+                    utils.info("A vous de jouer, choisissez une case !");
+                    break;
+                case this.PHASE_PLAY_OPPONENT:
+                    utils.info("A votre adversaire de jouer...");
+                    this.players[1].play();
+                    break;
             }
 
         },
@@ -168,7 +168,7 @@
                             });
                         }
                     }
-                // si on est dans la phase de jeu (du joueur humain)
+                    // si on est dans la phase de jeu (du joueur humain)
                 } else if (this.getPhase() === this.PHASE_PLAY_PLAYER) {
                     this.players[0].play(utils.eq(e.target), utils.eq(e.target.parentNode));
                 }
@@ -218,6 +218,9 @@
             this.players[0].renderTries(this.grid);
         },
         renderMiniMap: function () {
+            let cloneGrid = this.grid.cloneNode(true);
+            this.miniGrid.innerHTML = "";
+            this.miniGrid.appendChild(cloneGrid);
         }
     };
 
