5a1621e30727f531359c5c9db0818e9b1333b39b
Séraphin <seraphinbnt@gmail.com>
2024-04-04 11:51:41 +0200

    update player.js with jslint (2 errors left)

--- js/player.js
+++ js/player.js
@@ -4,14 +4,30 @@
 (function (global) {
     "use strict";
 
-    var ship = { dom: { parentNode: { removeChild: function () { } } } };
+    // var ship = { dom: { parentNode: { removeChild: function () { } } } };
 
     var player = {
-        grid: [],
-        tries: [],
-        fleet: [],
-        game: null,
+        activateNextShip: function () {
+            if (this.activeShip < this.fleet.length - 1) {
+                this.activeShip += 1;
+                return true;
+            } else {
+                return false;
+            }
+        },
         activeShip: 0,
+
+        clearPreview: function () {
+            this.fleet.forEach(function (ship) {
+                if (ship.dom.parentNode) {
+                    ship.dom.parentNode.removeChild(ship.dom);
+                }
+            });
+        },
+        fleet: [],
+
+        game: null,
+        grid: [],
         init: function () {
             // créé la flotte
             this.fleet.push(shipFactory.build(shipFactory.TYPE_BATTLESHIP));
@@ -23,16 +39,16 @@
             this.grid = utils.createGrid(10, 10);
             this.tries = utils.createGrid(10, 10);
         },
-        setGame: function (game) {
-            this.game = game;
-        },
+
         play: function (col, line, clickedCell, alreadyClicked) {
-            // appel la fonction fire du game, et lui passe une calback pour récupérer le résultat du tir
+            // appel la fonction fire du game, et lui passe
+            //  une calback pour récupérer le résultat du tir
             this.game.fire(this, col, line, _.bind(function (hasSucced) {
                 this.tries[line][col] = hasSucced;
             }, this), clickedCell, alreadyClicked);
         },
-        // quand il est attaqué le joueur doit dire si il a un bateaux ou non à l'emplacement choisi par l'adversaire
+         // quand il est attaqué le joueur doit dire si il a un bateaux
+        //  ou non à l'emplacement choisi par l'adversaire
         receiveAttack: function (col, line, callback) {
             var succeed = false;
 
@@ -42,54 +58,21 @@
             }
             callback.call(undefined, succeed);
         },
-        setActiveShipPosition: function (x, y) {
-            var ship = this.fleet[this.activeShip];
-            var i = 0;
+        // renderShips: function (grid) {
+        // },
+        renderTries: function (grid) {
+            this.tries.forEach(function (row, rid) {
+                row.forEach(function (val, col) {
 
-            let n = Math.floor(ship.getLife() / 2);
-           
+                    var node = grid.querySelector(".row:nth-child(" + (rid + 1) + ") " + ".cell:nth-child(" + (col + 1) + ")");
 
-            if(ship.isVertical === false) {
-                x -= n;
-                 // vérifie que l'emplacement du bateau est vide et existe bien dans la grille
-                while(i < ship.getLife()) {
-                    if(this.grid[y][x + i] !== 0) {
-                        return false;
+
+                    if (val === true) {
+                        node.style.backgroundColor = "#e60019";
+                    } else if (val === false) {
+                        node.style.backgroundColor = "#aeaeae";
                     }
-                    i += 1;
-                }
-
-                i = 0;
-
-                while (i < ship.getLife()) {                
-                    this.grid[y][x + i] = ship.getId();
-                    i += 1;
-                }
-            } else {
-                y -= n;
-                 // vérifie que l'emplacement du bateau est vide et existe bien dans la grille
-                 while(i < ship.getLife()) {
-                    if(this.grid[y + i] === undefined || this.grid[y + i][x] !== 0) {
-                        return false;
-                    }
-                    i += 1;
-                }
-
-                i = 0;
-
-                while (i < ship.getLife()) {                
-                    this.grid[y + i][x] = ship.getId();
-                    i += 1;
-                }
-            }
-
-            return true;
-        },
-        clearPreview: function () {
-            this.fleet.forEach(function (ship) {
-                if (ship.dom.parentNode) {
-                    ship.dom.parentNode.removeChild(ship.dom);
-                }
+                });
             });
         },
         resetShipPlacement: function () {
@@ -98,29 +81,55 @@
             this.activeShip = 0;
             this.grid = utils.createGrid(10, 10);
         },
-        activateNextShip: function () {
-            if (this.activeShip < this.fleet.length - 1) {
-                this.activeShip += 1;
-                return true;
-            } else {
-                return false;
-            }
-        },
-        renderTries: function (grid) {
-            this.tries.forEach(function (row, rid) {
-                row.forEach(function (val, col) {
-                    var node = grid.querySelector('.row:nth-child(' + (rid + 1) + ') .cell:nth-child(' + (col + 1) + ')');
+        setActiveShipPosition: function (x, y) {
+            var ship = this.fleet[this.activeShip];
+            var i = 0;
 
-                    if (val === true) {
-                        node.style.backgroundColor = '#e60019';
-                    } else if (val === false) {
-                        node.style.backgroundColor = '#aeaeae';
+            var n = Math.floor(ship.getLife() / 2);
+
+            // vérifie que l'emplacement du bateau est vide
+            //  et existe bien dans la grille
+            if (ship.isVertical === false) {
+                x -= n;
+
+                while (i < ship.getLife()) {
+                    if (this.grid[y][x + i] !== 0) {
+                        return false;
                     }
-                });
-            });
+                    i += 1;
+                }
+
+                i = 0;
+
+                while (i < ship.getLife()) {
+                    this.grid[y][x + i] = ship.getId();
+                    i += 1;
+                }
+            } else {
+                y -= n;
+
+                while (i < ship.getLife()) {
+                    if (this.grid[y + i] === undefined || this.grid[y + i][x] !== 0) {
+                        return false;
+                    }
+                    i += 1;
+                }
+
+                i = 0;
+
+                while (i < ship.getLife()) {
+                    this.grid[y + i][x] = ship.getId();
+                    i += 1;
+                }
+            }
+
+            return true;
         },
-        renderShips: function (grid) {
-        }
+
+        setGame: function (game) {
+            this.game = game;
+        },
+        tries: []
     };
 
     global.player = player;
