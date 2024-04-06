d30cfba8255db5de67f9e14b89fd8fb7d2bdd1ca
Séraphin <seraphinbnt@gmail.com>
2024-04-06 02:39:06 +0200

    [+] create difficult IA

--- js/computer.js
+++ js/computer.js
@@ -13,21 +13,133 @@
             var self = this;
             var shoot;
             // setTimeout(function () {
+                // shoot = self.difficultIA();
                 do {
-                    shoot = self.pickCoordinates();
+                    shoot = self.difficultIA();
                 } while (self.tries[shoot.y][shoot.x] !== 0)
+
+                console.log(shoot);
+
+
+                // do {
+                //     shoot = self.pickRandomCoordinates();
+                // } while (self.tries[shoot.y][shoot.x] !== 0)
                 
                 self.game.fire(this, shoot.x, shoot.y, function (hasSucced) {
                     self.tries[shoot.y][shoot.x] = hasSucced;
+                    let k = 0;
+                    while(k < self.tries.length) {
+                        console.log(self.tries[k]);
+                        k += 1;
+                    }
                 });
             // }, 2000);
         },
-        pickCoordinates: function () {
+        pickRandomCoordinates: function () {
             var coordinates = {
                 x: Math.floor(Math.random() * 10),
                 y: Math.floor(Math.random() * 10)
             };
             return coordinates;
+        },
+        difficultIA: function () {
+            console.log('new shoot')
+            // let k = 0;
+            // while(k < this.tries.length) {
+            //     console.log(this.tries[k]);
+            //     k += 1;
+            // }
+
+            var shoot;
+            var bestShoots = [];
+            var isVertical = false;
+            var isHorizontal = false;
+            function Shoot (x, y) {
+                this.x = x;
+                this.y = y;
+            }
+            // cherche les cases que l'ordinateur a touché
+            for(var y = 0; y < this.tries.length; y++) {
+                for(var x = 0; x < this.tries[y].length; x++) {
+                    if(this.tries[y][x] === true) {
+
+                        // vérifie si une autre case est touchée à coté pour attaquer dans le même sens
+                        if(x < this.tries[y].length - 1
+                            && this.tries[y][x + 1] === 0
+                            && this.tries[y][x - 1] !== undefined
+                            && this.tries[y][x - 1] === true) {
+                            bestShoots.push(new Shoot(x + 1, y));
+                        }
+                        if(x > 0 
+                            && this.tries[y][x - 1] === 0
+                            && this.tries[y][x + 1] !== undefined
+                            && this.tries[y][x + 1] === true) {
+                            bestShoots.push(new Shoot(x - 1, y));
+                        }
+                        if(y < this.tries.length - 1 
+                            && this.tries[y + 1][x] === 0
+                            && this.tries[y - 1] !== undefined
+                            && this.tries[y - 1][x] === true) {
+                            bestShoots.push(new Shoot(x, y + 1));
+                        }
+                        if(y > 0 
+                            && this.tries[y - 1][x] === 0
+                            && this.tries[y + 1] !== undefined
+                            && this.tries[y + 1][x] === true) {
+                            bestShoots.push(new Shoot(x, y - 1));
+                        }
+
+                        // vérifie si un sens a déja été trouvé (pour ne pas entourer le bateau)
+                        if((this.tries[y][x - 1] !== undefined
+                            && this.tries[y][x - 1] === true)
+                            || (this.tries[y][x + 1] !== undefined
+                            && this.tries[y][x + 1] === true)) {
+                                isHorizontal = true;
+                                console.log(isHorizontal)
+                        }
+
+                        if((this.tries[y - 1] !== undefined
+                            && this.tries[y - 1][x] === true)
+                            || (this.tries[y + 1] !== undefined
+                            && this.tries[y + 1][x] === true)) {
+                                isVertical = true;
+                                console.log(isVertical)
+                        }
+
+
+
+                        // ajoute toutes les cases à côté si aucune n'a été touchée
+                        if(bestShoots.length === 0) {
+                             if(x < this.tries[y].length - 1 
+                                && this.tries[y][x + 1] === 0
+                                && isVertical === false) {
+                                bestShoots.push(new Shoot(x + 1, y));
+                            }
+                            if(x > 0 && this.tries[y][x - 1] === 0
+                                && isVertical === false) {
+                                bestShoots.push(new Shoot(x - 1, y));
+                            }
+                            if(y < this.tries.length - 1 
+                                && this.tries[y + 1][x] === 0
+                                && isHorizontal === false) {
+                                bestShoots.push(new Shoot(x, y + 1));
+                            }
+                            if(y > 0 && this.tries[y - 1][x] === 0
+                                && isHorizontal === false) {
+                                bestShoots.push(new Shoot(x, y - 1));
+                            }
+                        }
+                       
+                    }
+                }
+
+                isHorizontal = false;
+                isVertical = false;
+            }
+            console.log(bestShoots);
+
+            shoot = bestShoots.length > 0 ? _.sample(bestShoots) : this.pickRandomCoordinates();
+            return shoot
         },
         isShipOk: function (callback) {
 
@@ -35,11 +147,11 @@
                while(!this.placement(ship));
             }, this);
 
-            let k = 0;
-            while(k < this.grid.length) {
-                console.log(this.grid[k]);
-                k += 1;
-            }
+            // let k = 0;
+            // while(k < this.grid.length) {
+            //     console.log(this.grid[k]);
+            //     k += 1;
+            // }
 
             // setTimeout(function () {
                 callback();
